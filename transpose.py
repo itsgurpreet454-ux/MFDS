d0d4c0d4dafb5209ec2474cf0d27ce4929a04ecb
itsgurpreet454-ux <its.gurpreet454@gmail.com>
2026-08-18 12:25:15 +0530

    Update transpose.py

--- transpose.py
+++ transpose.py
@@ -1,16 +1,16 @@
 def transpose(matrix):
 
   rows = len(matrix)
-  cols = len(matrix[0])
+  colms = len(matrix[0])
 
-  result = [[matrix[i][j] for i in range(rows)] for j in range(cols)]
+  output = [[matrix[i][j] for i in range(rows)] for j in range(cols)]
 
   return result
 
 
 matrix = [[1, 2, 3], [4, 5, 6]]
 
-transposed_matrix = transpose(matrix)
-print("Transposed Matrix :")
-for row in transposed_matrix:
+new_matrix = transpose(matrix)
+print("Transpose of  Matrix :")
+for row in new_matrix:
     print(row)
